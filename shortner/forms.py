990b5e7972369dd32a58f173ba49da6017221367
Anubhav Tiwari <anubhavtiwaricp@gmail.com>
2021-10-23 23:35:03 +0530

    Revert "Designed the Whole Project"
    This reverts commit c0a676a2759c7f5bfc13c6cee8592ff9c18599fc.
    
    This will revert previous commit

--- shortner/forms.py
+++ shortner/forms.py
@@ -6,10 +6,3 @@ class UrlShortner(ModelForm):
     class Meta:
         model = URL
         fields = ['full_url']
-        labels = {
-            "full_url": ""
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(UrlShortner, self).__init__(*args, **kwargs)
-        self.fields['full_url'].widget.attrs.update({'class': 'myfieldclass', 'placeholder': 'Enter the link here', 'label': ''})
